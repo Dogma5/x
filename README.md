9e76eedb8bb51706d783b23b22970a1f823fb533
Alex Page <alex@alexpage.com.au>
2021-01-24 21:04:07 -0800

    Engine and basic movement (#10)
    Co-authored-by: Trevor Brennan <treb.brennan@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+```
+///////////////////////
+======= FDR LOG =======
+Vessel: GU9881633
+Time:   16225621354
+Uplink: Out of range
+
+SOS SOS SOS QQQ
+GU98816 AVVENTURA
+ABANDONING SHIP
+1 known survivor
+
+///////////////////////
+```
+
 # x
-A game
+
+> A game built by @alex-page and @treb
+
+## Get started
+
+To play the game go to [url]
---
+++ engine/index.js
@@ -0,0 +1,90 @@
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d', {alpha: false});
+
+function setPixel(x, y, color) {
+	context.fillStyle = color;
+	context.fillRect(x, y, 1, 1);
+}
+
+function createSprite(path, width, height) {
+	const img = new Image();
+	img.src = path;
+	return {img, width, height};
+}
+
+function setSprite(x, y, sprite, spriteIndex) {
+	const {img, width, height} = sprite;
+	const cropX = ((spriteIndex * width) % img.width);
+	const cropY = Math.trunc(spriteIndex * width / img.width) * height;
+	context.drawImage(img, cropX, cropY, width, height, x, y, width, height);
+}
+
+function clearScreen() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+const btn = {
+	ArrowUp: false,
+	ArrowLeft: false,
+	ArrowDown: false,
+	ArrowRight: false,
+	Space: false,
+	Enter: false,
+	Escape: false
+};
+
+const updatePressed = (eventCode, isPressed) => {
+	if (eventCode in btn) {
+		btn[eventCode] = isPressed;
+	}
+};
+
+function gameLoop(update, draw) {
+	requestAnimationFrame(() => {
+		update();
+		draw();
+		gameLoop(update, draw);
+	});
+}
+
+function engine(options, update, draw) {
+	const {
+		width = 800,
+		height = 600,
+		responsive = false,
+		pageBackground = '#000',
+		id = 'game'
+	} = options;
+
+	canvas.id = id;
+	canvas.width = width;
+	canvas.height = height;
+	canvas.style.border = '4px solid #111';
+
+	canvas.style.imageRendering = 'crisp-edges';
+	canvas.style.imageRendering = 'pixelated';
+	canvas.style.width = `${width * 4}px`;
+	canvas.style.height = `${height * 4}px`;
+
+	if (responsive) {
+		canvas.style.maxWidth = '100%';
+		canvas.style.maxHeight = '100%';
+	}
+
+	document.body.style.background = pageBackground;
+	document.body.append(canvas);
+
+	document.addEventListener('keydown', event => updatePressed(event.code, true));
+	document.addEventListener('keyup', event => updatePressed(event.code, false));
+
+	gameLoop(update, draw);
+}
+
+export {
+	engine,
+	btn,
+	clearScreen,
+	setPixel,
+	setSprite,
+	createSprite
+};
